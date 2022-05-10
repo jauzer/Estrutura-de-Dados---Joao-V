69add45706499dae48fbc8e85f6738f072c1c316
jauzer <87881101+jauzer@users.noreply.github.com>
2022-05-10 02:30:55 -0300

    Create PontoCartesiano.java

---
+++ Atividade 1/PontoCartesiano.java
@@ -0,0 +1,30 @@
+public class PontoCartesiano {
+    private int xa, xb;
+    private int ya, yb;
+    private double distancia;
+    
+    public PontoCartesiano(int xa, int xb, int ya, int yb) {
+        this.xa = xa;
+        this.xb = xb;
+        this.ya = ya;
+        this.yb = yb;
+        this.distancia = distancia;
+    }
+    
+    public void setXa(int xa) {this.xa = xa;}
+    public void setXb(int xb) {this.xb = xb;}
+    public void setYa(int ya) {this.ya = ya;}
+    public void setYb(int yb) {this.yb = yb;}
+    
+    public int getXa() {return xa;}
+    public int getXb() {return xb;}
+    public int getYa() {return ya;}
+    public int getYb() {return yb;}
+    
+    public void calculaDistancia() {
+        int x;
+        int y;
+        //Math.sqrt
+        //Math.pow
+    }
+}
