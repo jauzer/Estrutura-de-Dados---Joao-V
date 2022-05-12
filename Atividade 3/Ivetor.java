3a11d0646911d13f2ad93cf3acff195aea1ef0ba
jauzer <87881101+jauzer@users.noreply.github.com>
2022-05-12 01:13:55 -0300

    create Ivetor.java - interface

---
+++ Atividade 3/Ivetor.java
@@ -0,0 +1,9 @@
+public interface Ivetor {
+
+    public void adiciona(String nome, int idade, double nota);
+    public int tamanho();
+    public boolean contem();
+    public boolean cheio(); //avisar quando o vetor fica cheio
+    public boolean remove(String nome);
+
+}
