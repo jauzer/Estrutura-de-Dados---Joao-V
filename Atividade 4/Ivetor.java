ef283029ba41d12e58fee98cf0b57be218e56ec3
jauzer <87881101+jauzer@users.noreply.github.com>
2022-05-12 02:02:48 -0300

    Create Ivetor.java

---
+++ Atividade 4/Ivetor.java
@@ -0,0 +1,9 @@
+public interface Ivetor {
+    public void adiciona(Object elem);
+    public int tamanho();
+    public boolean contem();
+    public boolean cheio(); //avisar quando o vetor fica cheio
+    public boolean remove(Object elem);
+    public void imprimirVetor();
+
+}
