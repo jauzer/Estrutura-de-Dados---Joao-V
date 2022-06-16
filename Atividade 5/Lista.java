9027727d2b936eb31413ae181772290342e35186
jauzer <87881101+jauzer@users.noreply.github.com>
2022-06-16 19:06:03 -0300

    Create Lista.java

---
+++ Atividade 5/Lista.java
@@ -0,0 +1,8 @@
+public interface Lista {
+    public void insereInicio(Object info);
+    public void insereFim(Object info);
+    public boolean estahVazia();
+    public boolean remove(Object info);
+    public int tamanho();
+    public void imprimir();
+}
