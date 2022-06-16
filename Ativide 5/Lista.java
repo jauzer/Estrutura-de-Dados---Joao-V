16534b0d7c4c3487ccf32d4b65fb326ecb650b8e
jauzer <87881101+jauzer@users.noreply.github.com>
2022-06-16 19:21:26 -0300

    Delete Ativide 5 directory

--- Ativide 5/Lista.java
+++
@@ -1,8 +0,0 @@
-public interface Lista {
-    public void insereInicio(Object info);
-    public void insereFim(Object info);
-    public boolean estahVazia();
-    public boolean remove(Object info);
-    public int tamanho();
-    public void imprimir();
-}
