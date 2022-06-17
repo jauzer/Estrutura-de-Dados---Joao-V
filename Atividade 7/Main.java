b1fac7c2fafe1ad692d5dc4e711b0eda1b029a2b
jauzer <87881101+jauzer@users.noreply.github.com>
2022-06-16 22:44:09 -0300

    Create Main.java

---
+++ Atividade 7/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Main
+{
+	public static void main(String[] args) {
+		TADVetor v = new TADVetor();
+		
+        v.adiciona(1000);
+        
+        v.mostraMaior();
+        //v.imprime(); //no momento coloquei pra imprimir os 10 primeiros
+        v.tamanho();
+        //v.contem(10);
+	}
+}
