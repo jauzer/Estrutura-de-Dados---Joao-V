8bf091e2725666c99284fd2119f16b6fcf02e5ef
jauzer <87881101+jauzer@users.noreply.github.com>
2022-06-27 21:38:34 -0300

    Create Main.java

---
+++ Atividade 11/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		FilaLista l = new FilaLista();
+		
+		l.add(10);
+		l.add("Joao");
+		l.add("Cirilo");
+		l.add("Maria");
+		l.add("Guilherme");
+		
+		l.imprime();
+		l.size();
+		
+	}
+}
