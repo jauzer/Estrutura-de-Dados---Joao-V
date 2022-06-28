821f0cdc7368c713d737cbce7af9580770916fa0
jauzer <87881101+jauzer@users.noreply.github.com>
2022-06-27 21:38:57 -0300

    Create IFila.java

---
+++ Atividade 11/IFila.java
@@ -0,0 +1,8 @@
+public interface IFila {
+    
+    public boolean add(Object info);
+    public boolean remove(Object info);
+    public boolean isEmpty();
+    public void imprime();
+    public void size(); //troquei pra void, pq sim
+}
