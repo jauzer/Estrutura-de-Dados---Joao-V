506988569441b060bb622e2d11b614385705cf7f
jauzer <87881101+jauzer@users.noreply.github.com>
2022-06-27 21:38:48 -0300

    Create Noh.java

---
+++ Atividade 11/Noh.java
@@ -0,0 +1,13 @@
+public class Noh {
+    private Object info;
+    private Noh prox;
+    
+    public Noh (Object info){
+        this.info = info;
+        this.prox = null;
+    }
+    
+    public Object getInfo() {return info;}
+    public Noh getProx() {return prox;}
+    public void setProx(Noh n) {this.prox = n;}
+}
