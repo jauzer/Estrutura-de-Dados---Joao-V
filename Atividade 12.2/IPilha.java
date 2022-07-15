40e8e1eda0724848723a47d6adb122599a534b92
jauzer <87881101+jauzer@users.noreply.github.com>
2022-07-14 21:40:24 -0300

    Create IPilha.java

---
+++ Atividade 12.2/IPilha.java
@@ -0,0 +1,8 @@
+public interface IPilha {
+    public boolean push(Object info);
+    public Object pop();
+    public boolean ehPalindromo(String info);
+    //public Object top(); //retorna sem removê-lo
+    //public boolean isEmpty();
+    //public int size();
+}
