0348da8b8e994549ccb56855e81faa2f8189ff6e
jauzer <87881101+jauzer@users.noreply.github.com>
2022-07-18 16:34:23 -0300

    Create LSE.java

---
+++ Atividade 13/LSE.java
@@ -0,0 +1,7 @@
+public class LSE {
+    private LSE[] vetorLista; //vetor de listas e não mais de Aluno
+    
+    public LSE() {
+        this.vetorLista = new LSE[10]; //mas a LSE é de alunos!
+    }
+}
